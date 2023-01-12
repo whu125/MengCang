6a5cfd9103be3652e4a05ddfb257c3c6efc5fc8d
whu125 <2691806221@qq.com>
2023-01-12 23:56:01 +0800

    first commit

---
+++ pages/index/index.vue
@@ -0,0 +1,36 @@
+<template>
+
+	<div class="bgBox">
+		<!-- 似乎是太大了 在微信小程序里面显示不了 暂定做成app -->
+		<img src="../../static/bg.jpg" alt="" class="bg">
+
+		<div class="test"></div>
+	</div>
+
+
+</template>
+<script setup lang="ts">
+</script>
+<style lang="scss" scoped>
+	.bgBox {
+		// padding-top: 20%;
+		position: relative;
+		height: 100%;
+	}
+
+	.bg {
+
+		width: 100%;
+		height: 100%;
+	}
+
+	.test {
+		height: 30px;
+		width: 100px;
+		position: absolute;
+		top: 560rpx;
+		left: 70rpx;
+		background: pink;
+		z-index: 999;
+	}
+</style>
---
+++ unpackage/dist/dev/mp-weixin/pages/index/index.js
@@ -0,0 +1,8 @@
+"use strict";
+const common_vendor = require("../../common/vendor.js");
+const _sfc_main = {};
+function _sfc_render(_ctx, _cache) {
+  return {};
+}
+const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__scopeId", "data-v-1cf27b2a"], ["__file", "C:/Users/86189/Desktop/\u524D\u7AEF\u9879\u76EE/dream of the red chamber/pages/index/index.vue"]]);
+wx.createPage(MiniProgramPage);
